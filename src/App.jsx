7a3bc825146e6384ccbd41ae0a54b803be9ad17a
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-02-05 09:57:20 +0200

    remove files and basic css install fonts

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div className="card-followers">
+      <div className="card_header">
+        <img src="images/icon-twitter.svg" />
+        <span className="card_name">@nathanf</span>
+      </div>
+      <h2>1987</h2>
+      <p>FOLLOWERS</p>
+      <div className="card_rate">
+        <img src="images/icon-up.svg" />
+        <span>12 Today</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
